c0a7419664cb104737f74b15725e7332386d8682
Chris Zubak-Skees <chriszs@gmail.com>
2022-02-04 14:32:18 -0500

    feat: replay requests, use only existing scrapers

---
+++ cli.py
@@ -0,0 +1,55 @@
+import logging
+from pathlib import Path
+
+import click
+from warn import Runner, utils
+import vcr
+
+WARN_DATA_DIR = Path("data")
+WARN_CACHE_DIR = Path("data/files")
+
+
+@click.command()
+@click.argument("scrapers", nargs=-1, required=False)
+@click.option(
+    "--list",
+    default=False,
+    help="Lists all scrapers",
+    is_flag=True,
+)
+@click.option(
+    "--record_mode",
+    "-r",
+    default="new_episodes",
+    # see https://vcrpy.readthedocs.io/en/latest/usage.html#record-modes
+    type=click.Choice(("once", "new_episodes", "none", "all"), case_sensitive=True),
+    help="Sets the mode for recording and replaying HTTP requests",
+)
+def main(
+    scrapers: list,
+    data_dir: Path = WARN_DATA_DIR,
+    cache_dir: Path = WARN_CACHE_DIR,
+    delete: bool = False,
+    log_level: str = "DEBUG",
+    record_mode: str = "new_episodes",
+    list: bool = False,
+):
+    if list:
+        print(",".join([scraper.upper() for scraper in utils.get_all_scrapers()]))
+        return
+
+    # Set higher log-level on third-party libs that use DEBUG logging,
+    # In order to limit debug logging to our library
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.getLogger("pdfminer").setLevel(logging.WARNING)
+
+    # Local logging config
+    logging.basicConfig(level=log_level, format="%(asctime)s - %(name)s - %(message)s")
+
+    with vcr.use_cassette("data/cassettes/responses.yaml", record_mode=record_mode):
+        runner = Runner(data_dir, cache_dir)
+        runner.scrape(scrapers[0])
+
+
+if __name__ == "__main__":
+    main()
